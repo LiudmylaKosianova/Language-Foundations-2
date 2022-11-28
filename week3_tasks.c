5fdc1b0e1bd2dc6f78a5c04363d3eaf1ae93d1ad
Luda <108550003+Snowwhiteswan@users.noreply.github.com>
2022-11-28 21:22:07 +0100

    Update week3_tasks.c

--- week3_tasks.c
+++ week3_tasks.c
@@ -1,3 +1,30 @@
+/*Your job is to find the length of the longest word in a text with no punctuation or special characters of any kind - 
+only contains words. To do so, please write a C-program that takes as a input first the number of words in a text, 
+followed by all of the words in the text. The output of your program should be the length of the longest word in the text.
+To simplify your program, you can assume that the longest word will not exceed 100 characters.*/
+
+#include <stdio.h>
+int main (void){
+    int number = 0;
+    int l = 0;
+    int maxi = 0;
+    char word[101];
+    scanf("%d", &number);
+    for (int i=0;i<number;i++){
+        scanf("%s", word);
+        l=0;
+        while(word[l]!='\0'){
+            l++;
+        }
+        if(l>maxi){
+            maxi = l;
+        }
+    }
+    printf("%d",maxi);
+    return 0;
+}
+
+
 /*Your job is to write a C-program that will tell a fan which line to stand in.
 To simplify the program, you may assume that student names are less than 50 characters long and contain no spaces. 
 Your program should output an integer value (1 or 2) depending on whether the fan should join line 1 (name has even number of letters)
