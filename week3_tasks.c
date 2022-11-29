09c2c1e1709fd1d8027c58a3498ccb36f908019b
Luda <108550003+Snowwhiteswan@users.noreply.github.com>
2022-11-29 11:13:00 +0100

    Update week3_tasks.c

--- week3_tasks.c
+++ week3_tasks.c
@@ -1,3 +1,40 @@
+/*You are conducting a linguistic study and are interested in finding words that contain the letter 't' or 'T' in the first half of the word
+(including the middle letter if there is one). Specifically, if the first half of the word does contain a 't' or a 'T', your program should output
+a 1. If the first half does not contain the letter 't' or 'T', but the second half does, then your program should output a 2. 
+Otherwise, if there is no 't' or 'T' in the word at all, your program's output should be -1. You may assume that the word entered does not have more
+than 50 letters.*/
+
+#include <stdio.h>
+int main(void){
+    char word[50];
+    int i=0;
+    int length=0;
+    int foundt=0;
+    scanf("%s", word);
+    while (word[i]!='\0'){
+        i++;
+        length++;
+    }
+    i=0;
+    while (!foundt && word[i]!='\0'){
+        if(word[i]=='t' || word[i]=='T'){
+            foundt=1;
+            if (i<=length/2){
+                printf("%d",1);
+                }else{
+                printf("%d",2);
+            }
+        }else{
+            i++;
+        }
+    }
+    if (!foundt){
+        printf("%d", -1);
+    }
+    return 0;
+}
+
+
 /*Your job is to find the length of the longest word in a text with no punctuation or special characters of any kind - 
 only contains words. To do so, please write a C-program that takes as a input first the number of words in a text, 
 followed by all of the words in the text. The output of your program should be the length of the longest word in the text.
